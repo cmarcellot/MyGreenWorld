d39d3dd102c9040f710f33654bde254cf47b6bf5
Franklin Anton <franklinantonpro@gmail.com>
2023-02-04 11:28:31 +0100

    Creation du projet avec Vuejs et Babylonjs

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # MyGreenWorld
+
+## NPM
+
+### Run the server
+npm run serve
+
+### Installer Babylon
+npm i @babylonjs/core
+npm i @babylonjs/loaders
+
+### Installer Vue 3
+npm i -g @vue/cli
+
+### Version actuelle Vue
+vue --version
---
+++ vuejs_app/src/components/BabylonScene.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <p>My Green World</p>
+    <canvas></canvas>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { MainScene } from '@/babylonjs/MainScene';
+
+export default defineComponent({
+  name: 'BabylonScene',
+  mounted() {
+    const canvas = document.querySelector("canvas") as HTMLCanvasElement;
+    new MainScene(canvas);
+   },
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+p {
+  color: black;
+  background: none;
+  margin-bottom: 1rem;
+  font-family: "Roboto Condensed";
+  font-weight: 400;
+  font-size: 2rem;
+}
+canvas {
+  width: 70%;
+  height: 70%;
+  border: none;
+  outline: none;
+}
+</style>

af02ab7a1d1680f45aa9830270d9a41922010637
Franklin Anton <franklinantonpro@gmail.com>
2023-04-12 17:34:21 +0200

    change size of skycraper

--- vuejs_app/src/babylonjs/MainScene.ts
+++ vuejs_app/src/babylonjs/MainScene.ts
@@ -204,7 +204,7 @@ export class MainScene {
                     break;
                 case "SkyscraperBuilding.glb":
                     mainMesh.position = new Vector3(100,0,-150);
-                    mainMesh.scaling = new Vector3(0.7,0.7,0.7);
+                    mainMesh.scaling = new Vector3(0.4,0.4,0.4);
                     mainMesh.rotation = new Vector3(0, 0, 0);
                     mainMesh.scaling.x *= -1;
                     break;

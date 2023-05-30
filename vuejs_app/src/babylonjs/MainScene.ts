c2c9cd113bf558992ae7e62c1e018487ee2e0fd8
Camille Marcellot <camillemarcellot@gmail.com>
2023-05-30 12:25:23 +0200

    Revert "feat: fix a visibility bug" (#51)
    * Revert "feat: fix a visibility bug (#45)"
    
    This reverts commit 9fb60e5c6587ec72c69701fbd03de882399d514c.
    
    * feats: re-fix visibility bugs
    refs: MGWT-117

--- vuejs_app/src/babylonjs/MainScene.ts
+++ vuejs_app/src/babylonjs/MainScene.ts
@@ -33,6 +33,14 @@ export class MainScene {
         private p_living : Living
 
     ) {
+        const cash = document.getElementById("cash") as HTMLElement;
+        const eco = document.getElementById("ecology") as HTMLElement;
+        const store = document.getElementById("store") as HTMLElement;
+
+        cash.hidden = true;
+        eco.hidden = true;
+        store.hidden  = true;
+            
         // We create an engine for the scene rendering
         this.engine = new Engine(canvas, true);
 

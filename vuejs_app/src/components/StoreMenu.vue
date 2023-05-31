8044d94d86c5f9f279b79a7440910072f05cc4aa
Camille Marcellot <camillemarcellot@gmail.com>
2023-05-31 07:43:20 +0200

    Mgwt 112 corriger les gains des batiments (#56)
    * feats: change calculation of price (price*1.15^boughtNumber)
    refs: MGWT-112
    
    * feats: modify price of structures
    refs: MGWT-112
    
    * feat: Add a space to the price at every thousand for better visibility
    refs: MGWT-112
    
    * feats: add abbreviations for high number (M for million)
    refs: MGWT-112
    
    * feats: Fix a gain décimal number
    refs: MGWT-112
    
    * feats: add abbrav and space in value of money (progcash)
    refs: MGWT-112
    
    * feats: change prix of improvements and value views
    refs: MGWT-112
    
    * feats: change bonus/malus ecologie
    refs: MGWT-112
    
    * feats: fix ecology % to 0 and 100
    refs: MGWT-112
    
    * feats: fix bug to storage.json
    refs: MGWT-112

--- vuejs_app/src/components/StoreMenu.vue
+++ vuejs_app/src/components/StoreMenu.vue
@@ -16,17 +16,21 @@
           <div class="container">
             <img class="image" :src="require('../../public/images/' + livingOrCommerce.picture)" alt="Image habitation ou commerce" />
             <div class="text-container">
-              <p class="price"> Prix: {{ livingOrCommerce.price }}</p>
+              <p v-if="Math.ceil(livingOrCommerce.price * Math.pow(1.15, livingOrCommerce.boughtNumber)) > 1000" class="price tooltip" :title="getTooltipText(livingOrCommerce.price * Math.pow(1.15, livingOrCommerce.boughtNumber), 'long')"> Prix: {{ formatNumber(Math.ceil(livingOrCommerce.price * Math.pow(1.15, livingOrCommerce.boughtNumber)), "short") }}
+                <span class="tooltip-indicator">?</span>
+              </p>
+              <p v-if="Math.ceil(livingOrCommerce.price * Math.pow(1.15, livingOrCommerce.boughtNumber)) < 1000" class="price tooltip"> Prix: {{ formatNumber(Math.ceil(livingOrCommerce.price * Math.pow(1.15, livingOrCommerce.boughtNumber)), "short") }}
+              </p>
               <hr class="separator" />
-              <p class="gainPerSec">Gain: {{ livingOrCommerce.gainPerSec }}/sec</p>
+              <p class="gainPerSec">Gain: {{ getTooltipText(livingOrCommerce.gainPerSec, 'short') }}/sec</p>
               <hr class="separator" />
               <p class="bonusEco">Écologie: {{ livingOrCommerce.ecoBonus }} %</p>
               <hr class="separator" />
-              <p class="livingCount">Nombre achetés : {{ livingOrCommerce.boughtNumber }}</p>
+              <p class="livingCount">Nombre achetés : {{ livingOrCommerce.boughtNumber.toLocaleString(undefined, { maximumFractionDigits: 0 }) }}</p>
             </div>
           </div>
           <button class="btn"
-          v-on:click="BuyLivingOrCommerce(city, livingOrCommerce)" :class="{ 'disabled': city.cashQuantity < livingOrCommerce.price }"> Acheter </button>
+          v-on:click="BuyLivingOrCommerce(city, livingOrCommerce)" :class="{ 'disabled': city.cashQuantity < Math.ceil(livingOrCommerce.price * Math.pow(1.15, livingOrCommerce.boughtNumber)) }"> Acheter </button>
         </div>
 
         <div class="nav nav-bloc" @click="toggleContent2">
@@ -57,7 +61,11 @@
             <div class="text-container">
               <p class="box">{{ improvement.desc }}</p>
               <hr class="separator" />
-              <p class="price"> Prix: {{ improvement.price }}</p>
+              <p v-if="Math.ceil(improvement.price * Math.pow(1.15, improvement.boughtNumber)) >= 1000" class="price tooltip" :title="getTooltipText(improvement.price * Math.pow(1.15, improvement.boughtNumber), 'long')"> Prix: {{ formatNumber(Math.ceil(improvement.price * Math.pow(1.15, improvement.boughtNumber)), "short") }}
+                <span class="tooltip-indicator">?</span>
+              </p>
+              <p v-if="Math.ceil(improvement.price * Math.pow(1.15, improvement.boughtNumber)) < 1000" class="price tooltip"> Prix: {{ formatNumber(Math.ceil(improvement.price * Math.pow(1.15, improvement.boughtNumber)), "short") }}
+              </p>
               <hr class="separator" />
               <p class="gainPerSec">Gain par seconde: {{ improvement.gainPerSec }}/sec</p>
               <hr class="separator" />
@@ -123,8 +131,7 @@ export default defineComponent({
         livings: [] as Living[],
         commerces: [] as Commerce[],
         energies: [] as Energy[],
-        improvements: [] as Improvement[],   
-
+        improvements: [] as Improvement[]
       };
     },
     methods: {
@@ -161,7 +168,39 @@ export default defineComponent({
         city.improvements = this.improvements;
         return this.improvements.sort((a, b) => a.price - b.price);
       },
+      
+      formatNumber(number: number, abbrevType: string) {
+        const abbreviationsShort = ['', 'K', 'M', 'B', 'T', 'Q', 'Qu', 'Sx', 'Sp', 'O', 'N'];
+        const abbreviationsLong = ['', 'millier', 'million', 'billion', 'trillion', 'quadrillion', 'quintillion', 'sextillions', 'septillion', 'octillion', 'nonillion'];
+        const base = 1000;
+        const decimals = 1;
 
+        if (number < base) {
+          return number.toFixed(decimals);
+        }
+
+        const abbreviations = abbrevType === 'long' ? abbreviationsLong : abbreviationsShort;
+        const exponent = Math.min(Math.floor(Math.log10(number) / 3), abbreviations.length - 1);
+        const scaledNumber = number / Math.pow(base, exponent);
+        const formattedNumber = scaledNumber.toFixed(decimals);
+
+        return formattedNumber + " " + abbreviations[exponent];
+      },
+
+      convertToDecimal(value: string): number {
+        return parseFloat(value);
+      },
+
+      getTooltipText(number: number, textType: string) {
+        const roundedNumber = number.toFixed(1);
+        const formattedNumber = parseFloat(roundedNumber).toLocaleString(undefined, { useGrouping: true });
+
+        if (textType === "short") {
+          return formattedNumber;
+        } else if (textType === "long") {
+          return formattedNumber + " (" + this.formatNumber(number, "long") + ")";
+        }
+      },
 
       //Compare the cashQuantity and the price 
       BuyLivingOrCommerce(city : City, livingOrCommerce: any) {
@@ -540,6 +579,25 @@ export default defineComponent({
   text-align: center;
   background-color: red;
   color: white;
+}
+
+.tooltip {
+  position: relative;
+}
+
+.tooltip-indicator {
+  position: absolute;
+  top: 50%;
+  right: 0px;
+  transform: translateY(-50%);
+  width: 20px;
+  height: 20px;
+  line-height: 20px;
+  text-align: center;
+  border-radius: 50%;
+  background-color: #f0f0f0;
+  font-weight: bold;
+  font-size: 14px;
 }
 
 </style>
